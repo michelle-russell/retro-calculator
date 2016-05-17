deaf92e1c5939efb17bbe9b27f2e790a4fdc33d6
Michelle Russell <Michelle-Mac@Michelles-MacBook-Pro.local>
2016-05-16 22:11:34 -0600

    Added sound. Need to fix button and backplate sizing.

--- calculator-retro/ViewController.swift
+++ calculator-retro/ViewController.swift
@@ -7,44 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     // Outlets
     @IBOutlet weak var lblScreen: UILabel!
     
+    // Create audio variable
+    var btnSound: AVAudioPlayer!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
     }
 
     // Actions
     @IBAction func onNumberButtonPress(btn: UIButton!){
-        
+        playAudio()
         
         
     }
     
     @IBAction func onAddButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
     
     @IBAction func onSubtractButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
 
     @IBAction func onMultiplyButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
     
     @IBAction func onDivideButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
     
     @IBAction func onEqualButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
     @IBAction func onClearButtonPress(sender: AnyObject) {
+        playAudio()
+        
     }
     
-    
+    func playAudio(){
+        
+        if btnSound.playing{
+            
+            btnSound.stop()
+            
+        }
+        btnSound.play()
+        
+    }
     
     
     
