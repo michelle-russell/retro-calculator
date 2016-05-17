4af975b974f137571d3b4c491d415a59783e617b
Michelle Russell <xlilangelgurl42x@yahoo.com>
2016-05-16 20:42:40 -0600

    Initial Commit

---
+++ calculator-retro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator-retro
+//
+//  Created by Michelle Russell on 5/16/16.
+//  Copyright © 2016 bananapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ calculator-retroUITests/calculator_retroUITests.swift
@@ -0,0 +1,36 @@
+//
+//  calculator_retroUITests.swift
+//  calculator-retroUITests
+//
+//  Created by Michelle Russell on 5/16/16.
+//  Copyright © 2016 bananapps. All rights reserved.
+//
+
+import XCTest
+
+class calculator_retroUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
